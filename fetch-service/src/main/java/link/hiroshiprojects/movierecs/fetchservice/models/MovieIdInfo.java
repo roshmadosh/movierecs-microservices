f37b11e5b70d050658e16fc3b8284ca81f50d75d
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-03-15 13:47:00 -0400

    genres resource impl

--- fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/models/MovieIdInfo.java
+++ fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/models/MovieIdInfo.java
@@ -1,49 +1,13 @@
 package link.hiroshiprojects.movierecs.fetchservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class MovieIdInfo {
     private boolean adult;
     private long id;
     private String originalTitle;
     private double popularity;
     private boolean video;
-
-    public boolean isAdult() {
-        return adult;
-    }
-
-    public void setAdult(boolean adult) {
-        this.adult = adult;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
-    }
-
-    public boolean isVideo() {
-        return video;
-    }
-
-    public void setVideo(boolean video) {
-        this.video = video;
-    }
 }
--- fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/resources/GenreResource.java
+++ fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/resources/GenreResource.java
@@ -1,4 +1,34 @@
 package link.hiroshiprojects.movierecs.fetchservice.resources;
 
+import link.hiroshiprojects.movierecs.fetchservice.models.Genres;
+import link.hiroshiprojects.movierecs.fetchservice.models.GenresObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fetch/genres")
 public class GenreResource {
+
+    @Value("${TMDB_API_KEY}")
+    private String KEY;
+    @Value("${source.url}")
+    private String HOST;
+    private final RestTemplate restTemplate;
+
+    public GenreResource(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping()
+    public List<GenresObject> getGenres() {
+        String url = String.format("%s/genre/movie/list?api_key=%s", HOST, KEY);
+        Genres genres = restTemplate.getForObject(url, Genres.class);
+
+        return genres.getGenres();
+    }
 }
