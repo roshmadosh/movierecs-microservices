95feff405d196470b6b2c23a2c376e9220a6fa53
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-03-16 13:46:16 -0400

    bugfix: post_path mapping

--- fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/FetchServiceApplication.java
+++ fetch-service/src/main/java/link/hiroshiprojects/movierecs/fetchservice/FetchServiceApplication.java
@@ -1,19 +1,26 @@
 package link.hiroshiprojects.movierecs.fetchservice;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class FetchServiceApplication {
 
 	@Bean
-	public ObjectMapper mapper() {
+	public ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+		mapper.configure(DeserializationFeature. FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 		return mapper;
 	}
@@ -21,7 +28,16 @@ public class FetchServiceApplication {
 
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
+
+		// configure so that it uses RestTemplate bean with our naming strategy
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+		MappingJackson2HttpMessageConverter jsonMessageConverter = new MappingJackson2HttpMessageConverter();
+		jsonMessageConverter.setObjectMapper(objectMapper());
+		messageConverters.add(jsonMessageConverter);
+		restTemplate.setMessageConverters(messageConverters);
+
+		return  restTemplate;
 	}
 
 
