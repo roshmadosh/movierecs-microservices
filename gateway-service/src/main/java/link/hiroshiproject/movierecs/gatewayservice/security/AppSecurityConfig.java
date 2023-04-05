c6df972decca28945ad18e5301f8544e2ba32577
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-04-05 08:31:59 -0400

    logout handler and logout success handler

--- gateway-service/src/main/java/link/hiroshiproject/movierecs/gatewayservice/security/AppSecurityConfig.java
+++ gateway-service/src/main/java/link/hiroshiproject/movierecs/gatewayservice/security/AppSecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,19 +22,26 @@ public class AppSecurityConfig {
     private ReactiveClientRegistrationRepository clientRegistrationRepository;
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        // this supposedly invalidates the session (https://docs.spring.io/spring-security/reference/reactive/authentication/logout.html)`
+        DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(
+                new WebSessionServerLogoutHandler(), new SecurityContextServerLogoutHandler()
+        );
        http
                .authorizeExchange(exchange -> exchange
                        .anyExchange().authenticated())
                .logout(logout -> logout
+                       .logoutHandler(logoutHandler)
                        .logoutSuccessHandler(oidcLogoutSuccessHandler()))
                .oauth2Login();
 
        return http.build();
     }
 
+    /**
+     * Docs say to do this for setting redirect url after logout.
+     * @link <a href="https://docs.spring.io/spring-security/reference/reactive/oauth2/login/advanced.html#webflux-oauth2-login-advanced-oidc-logout">...</a>
+     */
     private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        System.out.println("LOGOUTTTTTTTTTTTT");
-        log.info("LOGOUT commencing through handler...");
         OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
                 new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
 
