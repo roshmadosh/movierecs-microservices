851b7dfb04673f097085522a42ce1f97c2ed7040
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-04-05 12:30:11 -0400

    changed gateway from permitall to authenticated

--- gateway-resource-service/src/main/java/link/hiroshiprojects/movierecs/gatewayresourceservice/security/AppSecurityConfig.java
+++ gateway-resource-service/src/main/java/link/hiroshiprojects/movierecs/gatewayresourceservice/security/AppSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,7 +17,8 @@ public class AppSecurityConfig {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().permitAll()
+                        // authenticates on gateway level
+                        .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
