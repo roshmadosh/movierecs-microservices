9bad5cb83c40d90fb92c7954f3798d7b17ea88b0
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-04-07 14:21:55 -0400

    circuitbreaker for /users requests from admin service

--- admin-service/src/main/java/link/hiroshiprojects/movierecs/adminservice/resources/UsersResource.java
+++ admin-service/src/main/java/link/hiroshiprojects/movierecs/adminservice/resources/UsersResource.java
@@ -1,5 +1,6 @@
 package link.hiroshiprojects.movierecs.adminservice.resources;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import link.hiroshiprojects.movierecs.adminservice.models.AppUser;
 import link.hiroshiprojects.movierecs.adminservice.services.UsersService;
 import org.slf4j.Logger;
@@ -10,7 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +22,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 @PreAuthorize("hasRole('admin')")
+@CircuitBreaker(name = "users")
 public class UsersResource {
     private final Logger logger = LoggerFactory.getLogger(UsersResource.class);
     private final UsersService usersService;
@@ -31,38 +32,34 @@ public class UsersResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<AppUser>> getUsers(@AuthenticationPrincipal Jwt jwt) {
+    public List<AppUser> getUsers(@AuthenticationPrincipal Jwt jwt) {
        String adminToken = jwt.getTokenValue();
-       try {
-           List<AppUser> users = usersService.getUsers(adminToken);
-           return ResponseEntity.ok().body(users);
-       } catch (Exception e) {
-           e.printStackTrace();
-           return ResponseEntity.badRequest().body(Collections.emptyList());
-       }
-
+       return usersService.getUsers(adminToken);
     }
 
     /**
      * Register a user to the application.
      */
     @PostMapping
-    public ResponseEntity<AppUser> registerUser(@AuthenticationPrincipal Jwt jwt, @RequestBody Map<String, String> request) {
+    public ResponseEntity<AppUser> registerUser(@AuthenticationPrincipal Jwt jwt,
+                                                @RequestBody Map<String, String> request) {
+
         String adminToken = jwt.getTokenValue();
         String email = request.getOrDefault("email", null);
         if (email == null) {
-            logger.warn("Required request body parameter 'email' missing.");
+            String message = "Parameter 'email' not included in response body";
+            logger.warn(message);
             return ResponseEntity.badRequest().body(null);
         }
 
         // check if user exists in auth server
         if (!usersService.emailExists(adminToken, email)) {
-            logger.warn("Account not found in auth server: " + email);
+            String message = "Account '" + email + "' not found in server.";
+            logger.warn(message);
             return ResponseEntity.badRequest().body(null);
         }
-
         AppUser user = usersService.registerUser(adminToken, email);
-        return ResponseEntity.ok().body(user);
+        return ResponseEntity.ok(user);
     }
 
     /**
@@ -70,32 +67,34 @@ public class UsersResource {
      */
     @DeleteMapping
     public ResponseEntity<Map<String, Object>> deleteUser(@AuthenticationPrincipal Jwt jwt,
-                                                          @RequestBody Map<String, String> request) {
+                           @RequestBody Map<String, String> request) {
+
         String adminToken = jwt.getTokenValue();
         Map<String, Object> response = new HashMap<>();
         String email = request.getOrDefault("email", null);
         if (email == null) {
-            logger.warn("Required request body parameter 'email' missing.");
-            return ResponseEntity.badRequest().body(null);
+            String message = "Required request body parameter 'email' missing.";
+            logger.warn(message);
+            response.put("message", message);
+            return ResponseEntity.badRequest().body(response);
         }
         // check if user exists in auth server
         if (!usersService.emailExists(adminToken, email)) {
             String message = "Account not found in auth server: " + email;
             logger.warn(message);
-            response.put("success", false);
             response.put("message", message);
             return ResponseEntity.badRequest().body(response);
         }
 
         long userDeleted = usersService.deleteUser(adminToken, email);
         if (userDeleted != 1) {
-            logger.warn("Failed user deletion, returned item: " + userDeleted);
-            response.put("success", false);
-            response.put("message", "unknown error.");
+            String message = "Failed user deletion, returned item: " + userDeleted;
+            logger.warn(message);
+            response.put("message", message);
             return ResponseEntity.badRequest().body(response);
         }
-        response.put("success", true);
-        return ResponseEntity.ok().body(response);
+        response.put("message", "User '" + email + "' deleted successfully.");
+        return ResponseEntity.ok(response);
     }
 
 }
